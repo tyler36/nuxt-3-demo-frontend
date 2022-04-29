ee3b2e59caf6619cd17f5446a4dcc3445704e45f
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-04-29 16:15:40 +0900

    wip

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -1,28 +1,29 @@
 <template>
     <div class="post^container py-8">
         <h3 class="text-3xl font-bold">
-            <NuxtLink :to="`/posts/1`">Title of Post</NuxtLink>
+            <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
         </h3>
         <div class="flex items-center space-x-2 text-gray-700 post-meta">
-            <div>Feb 2, 2022</div>
+            <div>{{ post.created_at }}</div>
             <div>&middot;</div>
-            <div>Dave Lister</div>
+            <div>{{ post.user.name }}</div>
         </div>
         <div class="mt-4 leading-relaxed post-preview">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim
-            temporibus ipsum nobis dolorum libero optio amet et repellat esse
-            corrupti, non voluptatum ab assumenda vero maxime hic, itaque,
-            voluptas ullam ad necessitatibus illo illum velit debitis culpa?
-            Aperiam, provident itaque. Sequi tempora sunt ex temporibus. Aperiam
-            reprehenderit repellendus deserunt nihil.
+            {{ post.body }}
         </div>
 
         <div class="mt-4">
             <NuxtLink
-                :to="`/posts/1`"
+                :to="`/posts/${post.id}`"
                 class="inline-block px-4 py-4 text-white bg-blue-700 rounded hover:bg-blue-800"
                 >Read More</NuxtLink
             >
         </div>
     </div>
 </template>
+
+<script setup>
+const props = defineProps({
+    post: Object,
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,11 @@
     <div
         class="container w-1/2 py-8 mx-auto space-y-8 divide-y divide-gray-300"
     >
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
-        quae labore quibusdam veritatis. Adipisci in et cumque reprehenderit,
-        delectus repellendus natus omnis, autem a, quidem soluta? Quis quos sint
-        deserunt iste itaque, commodi cumque at odio error similique saepe minus
-        deleniti, consequatur quidem quam porro voluptas doloremque culpa
-        labore! Quisquam.
-        <PostItem v-for="n in 10" :key="n"></PostItem>
+        <PostItem v-for="post in posts" :key="post.id" :post="post"></PostItem>
     </div>
 </template>
+
+
+<script setup>
+const {data: posts} = await useFetch('http://127.0.0.1:8000/api/posts');
+</script>
